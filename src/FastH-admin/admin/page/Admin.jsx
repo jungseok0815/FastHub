de74cf9bf3bdaae3ab530861f0b54e9a993ae6fc
DESKTOP-CB5R3AI\USER <wjdtjr9401@naver.com>
2025-01-23 16:58:27 +0900

    in progress folerRefactoring

--- src/FastH-admin/admin/page/Admin.jsx
+++ src/FastH-admin/admin/page/Admin.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import AdminHeader from "../compoent/adminHeader";
+import AdminContent from "../compoent/adminContent";
+import AdminFooter from "../compoent/adminFooter";
+
 function admin(){
     return (
         <div className="admin">
           <AdminHeader />
+          <AdminContent />
+          <AdminFooter />
         </div>
     );
 }
